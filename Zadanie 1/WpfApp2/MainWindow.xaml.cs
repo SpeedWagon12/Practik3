9de2dd60b8b7c3b918189ea91ae7d008be8763d8
SpeedWagon12 <151126812+SpeedWagon12@users.noreply.github.com>
2023-11-24 09:22:25 +0300

    Add files via upload

---
+++ Zadanie 1/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (!double.TryParse(Box1.Text, out double a))
+            { MessageBox.Show("Поле только для чисел"); }
+
+            if (!double.TryParse(Box2.Text, out double b))
+            { MessageBox.Show("Поле только для чисел"); }
+            double c, g;
+            c = a / b;
+            g = a % b;
+            if (g == 0)
+            { Answer.Text += "частное от деление равно = " + c; }
+            else if (g != 0)
+            { Answer.Text += "остаток от деления" + g; }
+
+        }
+    }
+}
---
+++ Zadanie 3/Zadanie 3/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadanie_3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            if(!double.TryParse(Box1.Text,out double a))
+            { MessageBox.Show("Поле только дя чисел"); }
+
+            if (!double.TryParse(Box2.Text, out double b))
+            { MessageBox.Show("Поле только дя чисел"); }
+
+            if (!double.TryParse(Box3.Text, out double c))
+            { MessageBox.Show("Поле только дя чисел"); }
+
+            if (!double.TryParse(Box4.Text, out double d))
+            { MessageBox.Show("Поле только дя чисел"); }
+
+            if (a == b && b == c)
+            { Answer.Text += "4"; }
+
+            if (a == b && b == d)
+            { Answer.Text += "3"; }
+
+            if (a == c && d == c)
+            { Answer.Text += "2"; }
+
+            if (c == b && b == d)
+            { Answer.Text += "1"; }
+
+
+        }
+    }
+}
